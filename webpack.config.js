f78f81035978a4df82734d7859f99e1bccd9c471
Sivan Mehta <Sivanmehta2@gmail.com>
2018-10-24 20:35:51 -0700

    basic react app

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+  entry: [
+    path.join(__dirname, 'src', 'index.js')
+  ],
+  mode: 'development',
+  module: {
+    rules: [
+    {
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      use: [
+        {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/env', '@babel/react']
+          }
+        }
+      ],
+    }
+    ]
+  },
+  resolve: { extensions: ['.js', '.jsx'] },
+  output: {
+    path: path.resolve(__dirname, 'dist/'),
+    filename: 'build.js'
+  },
+  plugins: process.env.NODE_ENV === 'production' ?  [] : [new webpack.HotModuleReplacementPlugin()]
+};
