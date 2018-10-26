eb8bc6cfdb53668e59b34ae596355b8974621552
Sivan Mehta <smehta@godaddy.com>
2018-10-26 09:09:50 -0700

    square tests

--- test/square.test.js
+++ test/square.test.js
@@ -1,5 +1,42 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import assume from 'assume';
+import Square from '../src/square';
+
+const props = {
+  color: {
+    red: 32,
+    green: 48,
+    blue: 160
+  },
+  width: 100,
+  height: 200
+};
+
 describe('Square', function () {
-  it('should set the viewbox correctly');
-  it('should set the correct height and width');
-  it('should set the correct fill');
+  let square;
+  beforeEach(function () {
+    square = mount(<Square { ...props }/>);
+  });
+  afterEach(function () {
+    square.unmount();
+  });
+
+  it('should set the viewbox correctly', function () {
+    const viewBox = square.find('svg').prop('viewBox');
+    assume(viewBox).equals('0 0 100 200');
+  });
+
+  it('should set the correct height and width', function () {
+    const height = square.find('rect').prop('height');
+    const width = square.find('rect').prop('width');
+
+    assume(height).equals(200);
+    assume(width).equals(100);
+  });
+
+  it('should set the correct fill', function () {
+    const fill = square.find('rect').prop('fill');
+    assume(fill).equals('#2030a0');
+  });
 });
